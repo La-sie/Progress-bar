62f02ed601912936618a261adddf5795e36fd248
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-10-26 01:41:49 -0700

    Add files via upload

---
+++ progress.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById('progress');
+const prevBtn = document.getElementById('prev');
+const nextBtn= document.getElementById('next');
+const circle = document.querySelectorAll('.circle');
