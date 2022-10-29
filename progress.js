931edb1400edd1becbb8084876acc8d39a39734d
Sellasie <fsdjogatse@st.ug.edu.gh>
2022-10-29 01:33:44 -1200

    Create progress.js

--- progress.js
+++ progress.js
@@ -1,36 +1,53 @@
 const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
-const nextBtn= document.getElementById('next');
+const nextBtn = document.getElementById('next');
 const circle = document.querySelectorAll('.circle');
+ 
 
 let currentActive = 1;
 
 nextBtn.addEventListener('click', () => {
-     currentActive++;
-     
-     if (currentActive > circle.length){
-         currentActive = circle.length;
-       }
+   currentActive++;
+
+   if(currentActive > circle.length) {
+    currentActive = circle.length;
+   }
 })
 
-prevBtn.addEventListener('click', () => {
-      currentActive--;
+prevBtn.addEventListener ('click', () => {
+    currentActive--;
+
+    if(currentActive < 1 ){
+        currentActive = 1; 
+    }
+
+    update();
+})
+
+function update() {
+    circle.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active');
+        }
+        else{
+            circle.classList.remove('active');
+        }
+    })
+
     
-      if(currentActive < 1 ) {
-      currentActive = 1;
-}
-      update();
-})
+const active = document.querySelectorAll('.active');
 
-function update() { 
-     circle.forEach(( circle, idx) => { 
-     if(idx < currentActive) { 
-         circle.classList.add('active');
-     }
-     else { 
-          circle.classList.remove('active');
-     }
-})
+progress.style.width = (active.length - 1 ) / (circle.length) * 100 + '%';
 
-const active = document.querySelectorAll(.active');
+ if (currentActive ===1){
+    prevBtn.disabled = true;
+ }
+    else if ( currentActive === circle.length){
+    nextBtn.disabled = true;
+    }
+    else{
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
+    }
 }
+
